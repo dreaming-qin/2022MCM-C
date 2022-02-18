99c2001e70f7e83c6cf9016a448f90009e32bb7c
dreaming_qin <18697951462@qq.com>
2022-02-19 00:06:05 +0800

    LSTM+VaR

--- problem1.py
+++ problem1.py
@@ -1,3 +1,4 @@
+from problem1_al import VaR, create_dataset, get_LSTM_model, get_train_and_test_set,predict_future
 from torch import dropout
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,9 +11,11 @@ from keras.models import Sequential, load_model
 from sklearn.preprocessing import MinMaxScaler
 from keras.layers import Dropout
 
+
+
 # 改
-# df = pd.read_csv('../LBMA-GOLD.csv', engine='python', skipfooter=3)
-df = pd.read_csv('../BCHAIN-MKPRU.csv', engine='python', skipfooter=3)
+df = pd.read_csv('../LBMA-GOLD.csv', engine='python', skipfooter=3)
+# df = pd.read_csv('../BCHAIN-MKPRU.csv', engine='python', skipfooter=3)
 
 dataset = df.iloc[:,1].values
 dataset=np.reshape(dataset,(len(dataset),-1))
@@ -21,74 +24,73 @@ dataset=np.reshape(dataset,(len(dataset),-1))
 dataset = dataset.astype('float32')
 #归一化 在下一步会讲解
 scaler = MinMaxScaler(feature_range=(0, 1))
-dataset = scaler.fit_transform(dataset)
+# dataset = scaler.fit_transform(dataset)
 
+# 获得训练集和测试集
 train_size = int(len(dataset)*0.9)
-# train_size =1000
-trainlist = dataset[:train_size]
-# 改
-testlist=dataset[100:]
-# testlist = dataset[train_size:]
-
-def create_dataset(dataset, timestep):
-    dataX, dataY = [], []
-    for i in range(len(dataset)-timestep-1):
-        a = dataset[i:(i+timestep)]
-        dataX.append(a)
-        dataY.append(dataset[i + timestep])
-    return np.array(dataX),np.array(dataY)
 #训练数据太少 timestep并不能过大
-timestep = 100
-trainX,trainY  = create_dataset(trainlist,timestep)
-testX,testY = create_dataset(testlist,timestep)
-
+timestep = 30
+trainX,trainY,testX,testY = get_train_and_test_set(dataset,train_size,timestep)
 trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
 testX = np.reshape(testX, (testX.shape[0], testX.shape[1] ,1 ))
 
-# create and fit the LSTM network
-model = Sequential()
-# model.add(Dropout(0.2, input_shape=(None,1,)))
-model.add(LSTM(12, input_shape=(None,1),dropout=0.05))
-model.add(Dense(1,activation='elu'))
-model.compile(loss='mean_squared_error', optimizer='adam')
+# 创建LSTM
+model = get_LSTM_model()
+
 # 训练模型
 # 改
 # model = load_model(os.path.join("DATA","LSTM_gold" + ".h5"))
 # model = load_model(os.path.join("DATA","LSTM_bit" + ".h5"))
-model.fit(trainX, trainY, epochs=500, batch_size=100, verbose=2)
-# a=1
+model.fit(trainX, trainY, epochs=4000, batch_size=100, verbose=2)
 # 改
-model.save(os.path.join("DATA","LSTM_bit" + ".h5"))
-# model.save(os.path.join("DATA","LSTM_gold" + ".h5"))
+# model.save(os.path.join("DATA","LSTM_bit" + ".h5"))
+model.save(os.path.join("DATA","LSTM_gold" + ".h5"))
 
-# make predictions
 
-testPredict = model.predict(testX)
-
-#反归一化，获得结果图片
-
-testPredict = scaler.inverse_transform(testPredict).tolist()
-value=dataset[:len(dataset)-len(testPredict)]
-value=scaler.inverse_transform(value).tolist()
-value=value+testPredict
-
-date=df.iloc[:len(value),0].values.tolist()
-
-real_value=scaler.inverse_transform(dataset).tolist()
-
-dic={'Date':np.reshape(date,(len(date))),'predict value':np.reshape(value,(len(value))),
+# 预测
+# 预测天数
+days=15
+# 从第几天开始
+start_day=30
+test=dataset[start_day-timestep:start_day]
+testPredict=predict_future(days,test,model).tolist()
+# testPredict = scaler.inverse_transform(testPredict).tolist()
+testPredict=np.reshape(df.iloc[:start_day,1].values,(start_day,1)).tolist()+testPredict
+date=df.iloc[:start_day+days,0].values.tolist()
+real_value=df.iloc[:start_day+days,1].values.tolist()
+dic={'Date':np.reshape(date,(len(date))),'predict value':np.reshape(testPredict,(len(testPredict))),
 'real value':np.reshape(real_value,(len(real_value))) }
 # 改
-# pd.DataFrame(dic).to_csv('../result/黄金趋势(100天后).csv',index=False)
-pd.DataFrame(dic).to_csv('../result/比特币趋势(100天后).csv',index=False)
+pd.DataFrame(dic).to_csv('../result/黄金预测15天趋势.csv',index=False)
+# pd.DataFrame(dic).to_csv('../result/比特币预测15天趋势.csv',index=False)
 
 
 
-# plt.figure()
-# plt.plot(trainlist,'g-',label='dwell',color='red')
-# plt.legend(loc='best')
-# plt.savefig('2.png')
+# #获得每一天的预测结果
+# testPredict = model.predict(testX)
+# testPredict = scaler.inverse_transform(testPredict).tolist()
+# value=dataset[:len(dataset)-len(testPredict)]
+# value=scaler.inverse_transform(value).tolist()
+# value=value+testPredict
+
+# date=df.iloc[:len(value),0].values.tolist()
+
+# real_value=scaler.inverse_transform(dataset).tolist()
+
+# dic={'Date':np.reshape(date,(len(date))),'predict value':np.reshape(value,(len(value))),
+# 'real value':np.reshape(real_value,(len(real_value))) }
+# # 改
+# pd.DataFrame(dic).to_csv('../result/黄金趋势(30天后).csv',index=False)
+# # pd.DataFrame(dic).to_csv('../result/比特币趋势(30天后).csv',index=False)
+
+
+# var模型获得p2
+data = df.iloc[:,1].values
+# 将整型变为float
+data = data.astype('float32')
+p2=VaR(data,alpha=0.5)/data[len(data)-1]*days
 
 
 
 
+# 投资
---
+++ problem1_al.py
@@ -0,0 +1,62 @@
+
+from torch import dropout
+import numpy as np
+import matplotlib.pyplot as plt
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import  pandas as pd
+import  os
+from keras.models import Sequential, load_model
+from sklearn.preprocessing import MinMaxScaler
+from keras.layers import Dropout
+
+
+def predict_future(days,data,model):
+    ans=[0 for i in range(days)]
+    var1=np.reshape(data,(len(data)))
+    for i in range(days):
+        ans[i]=model.predict(np.reshape(var1,(1,len(var1),-1)))
+        var1=var1.tolist()
+        var1.append(ans[i][0][0])
+        var1=np.delete(var1,0)
+    return np.reshape(ans,(len(ans),1)) 
+
+def create_dataset(dataset, timestep):
+    dataX, dataY = [], []
+    for i in range(len(dataset)-timestep-1):
+        a = dataset[i:(i+timestep)]
+        dataX.append(a)
+        dataY.append(dataset[i + timestep])
+    return np.array(dataX),np.array(dataY)
+
+def get_train_and_test_set(dataset,train_size=1000,timestep=30):
+    # 获得训练集和测试集
+    # train_size =1000
+    trainlist = dataset[:train_size]
+    # 改
+    # testlist=dataset[30:]
+    testlist = dataset[train_size:]
+    #训练数据太少 timestep并不能过大
+    trainX,trainY  = create_dataset(trainlist,timestep)
+    testX,testY = create_dataset(testlist,timestep)
+    return trainX,trainY,testX,testY
+
+def get_LSTM_model():
+    model = Sequential()
+    # model.add(Dropout(0.2, input_shape=(None,1,)))
+    model.add(LSTM(6, input_shape=(None,1)))
+    model.add(Dense(1,activation='elu'))
+    model.compile(loss='mean_squared_error', optimizer='adam')
+    return model
+
+# data是1*n的ndarray向量
+def VaR(data,alpha=0.7):
+    data=data if len(data)<=101 else data[len(data)-101:]
+    sub=np.array([data[i+1]-data[i] for i in range(len(data)-1)])
+    sub_sort=np.sort(sub,axis=0)
+    return sub_sort[int(len(sub)*(1-alpha))]
+
+
+        
+    
