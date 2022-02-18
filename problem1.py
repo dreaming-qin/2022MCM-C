7f89d9019f7ab9311e5ae221016db875bfe2241d
dreaming_qin <18697951462@qq.com>
2022-02-18 13:57:25 +0800

    三次指数平滑

--- problem1.py
+++ problem1.py
@@ -1,81 +1,40 @@
-from scipy import stats
-import pandas as pd
+from turtle import color
 import matplotlib.pyplot as plt
-import statsmodels.api as sm
-from statsmodels.tsa.arima.model import ARIMA
-from statsmodels.graphics.tsaplots import plot_predict
-plt.rcParams['font.sans-serif']=['simhei']#用于正常显示中文标签
-plt.rcParams['axes.unicode_minus']=False#用于正常显示负号
+import pandas as pd
+
+def predict_value_with_exp_smoothing_3(data,params,T,predict_step):
+    a,r,b=params[0],params[1],params[2]
+    # 初始化S,B,I
+    S=[data[i] if i<=T else 0 for i in range(len(data))]
+    B=[data[i+1]-data[i] if i<=T else 0 for i in range(len(data))]
+    I=[1 if i<=T else 0 for i in range(len(data))]
+    for i in range(T+1,len(data)):
+        S[i]=a*(data[i]/I[i-T])+(1-a)*(S[i-1]+B[i-1])
+        B[i]=r*(S[i]-S[i-1])+(1-r)*B[i-1]
+        I[i]=b*(data[i]/S[i])+(1-b)*I[i-T]
+    len_data=len(data)-1
+    for i in range(1,predict_step+1):
+        data.append((S[len_data]+i*B[len_data])*I[len_data-T+(i%T)])
+    return data
 
 
 
-data = pd.read_csv('../BCHAIN-MKPRU.csv', encoding='utf-8', index_col='Date')
-data.index = pd.to_datetime(data.index)  # 将字符串索引转换成时间索引
+df=pd.read_csv('../BCHAIN-MKPRU.csv')
+dataset= df.iloc[:,1].tolist()
 
-# data.plot(figsize=(12,8),marker='o',color='black',ylabel='客运量')#画图
-# plt.show()
+plt.figure()
+plt.plot(dataset,'g-',label='dwell')
+plt.legend(loc='best')
+plt.savefig('1.png')
 
-# sm.tsa.adfuller(data,regression='c')
-# sm.tsa.adfuller(data,regression='nc')
-# sm.tsa.adfuller(data,regression='ct')
-
-diff=data.diff(1)
-diff.dropna(inplace=True)
-diff.plot(figsize=(12,8),marker='o',color='black')#画图
-# plt.show()
-
-fig = plt.figure(figsize=(12,8))
-ax1 = fig.add_subplot(211)
-fig = sm.graphics.tsa.plot_acf(diff.values.squeeze(), lags=12, ax=ax1)#自相关系数图1阶截尾,决定MA（1）
-ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_pacf(diff, lags=12, ax=ax2)#偏相关系数图1阶截尾,决定AR（1）
-
-model = ARIMA(data, order=(1, 1, 1)).fit()#拟合模型
-model.summary()#统计信息汇总
-
-#系数检验
-params=model.params#系数
-tvalues=model.tvalues#系数t值
-bse=model.bse#系数标准误
-pvalues=model.pvalues#系数p值
-
-#绘制残差序列折线图
-resid=model.resid#残差序列
-fig = plt.figure(figsize=(12,8))
-ax = fig.add_subplot(111)
-ax = model.resid.plot(ax=ax)
-
-#计算模型拟合值
-# fit=model.predict(exog=data[['TLHYL']])
-
-#8.1.检验序列自相关
-sm.stats.durbin_watson(model.resid.values)#DW检验：靠近2——正常；靠近0——正自相关；靠近4——负自相关
-
-#8.2.AIC和BIC准则
-model.aic#模型的AIC值
-model.bic#模型的BIC值
-
-#8.3.残差序列正态性检验
-stats.normaltest(resid)#检验序列残差是否为正态分布
-#最终检验结果显示无法拒绝原假设，说明残差序列为正态分布，模型拟合良好
-
-#8.4.绘制残差序列自相关图和偏自相关图
-fig = plt.figure(figsize=(12,8))
-ax1 = fig.add_subplot(211)
-fig = sm.graphics.tsa.plot_acf(resid.values.squeeze(), lags=12, ax=ax1)
-ax2 = fig.add_subplot(212)
-fig = sm.graphics.tsa.plot_pacf(resid, lags=12, ax=ax2)
-#如果两图都零阶截尾，这说明模型拟合良好
+dataset=predict_value_with_exp_smoothing_3(data=dataset,params=[0.2,0.05,0.9],T=365,predict_step=100) 
+plt.figure()
+plt.plot(dataset,'g-',label='dwell',color='red')
+plt.legend(loc='best')
+plt.savefig('2.png')
+plt.close()
 
 
-#预测至2016年的数据。由于ARIMA模型有两个参数，至少需要包含两个初始数据，因此从2006年开始预测
-predict = model.predict('2016', '2022', dynamic=True)
-print(predict)
-
-#画预测图及置信区间图
-fig, ax = plt.subplots(figsize=(10,8))
-fig = plot_predict(model, start='2016', end='2022', ax=ax)
-legend = ax.legend(loc='upper left')
 
 
 
