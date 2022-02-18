d40dca6dbc44df79c2d7f2b754170385af60f5e1
dreaming_qin <18697951462@qq.com>
2022-02-18 15:01:42 +0800

    LSTM使用成功

--- problem1.py
+++ problem1.py
@@ -1,40 +1,77 @@
-from turtle import color
+import numpy as np
 import matplotlib.pyplot as plt
-import pandas as pd
-
-def predict_value_with_exp_smoothing_3(data,params,T,predict_step):
-    a,r,b=params[0],params[1],params[2]
-    # 初始化S,B,I
-    S=[data[i] if i<=T else 0 for i in range(len(data))]
-    B=[data[i+1]-data[i] if i<=T else 0 for i in range(len(data))]
-    I=[1 if i<=T else 0 for i in range(len(data))]
-    for i in range(T+1,len(data)):
-        S[i]=a*(data[i]/I[i-T])+(1-a)*(S[i-1]+B[i-1])
-        B[i]=r*(S[i]-S[i-1])+(1-r)*B[i-1]
-        I[i]=b*(data[i]/S[i])+(1-b)*I[i-T]
-    len_data=len(data)-1
-    for i in range(1,predict_step+1):
-        data.append((S[len_data]+i*B[len_data])*I[len_data-T+(i%T)])
-    return data
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.layers import LSTM
+import  pandas as pd
+import  os
+from keras.models import Sequential, load_model
+from sklearn.preprocessing import MinMaxScaler
 
 
-
-df=pd.read_csv('../BCHAIN-MKPRU.csv')
-dataset= df.iloc[:,1].tolist()
+dataframe = pd.read_csv('../BCHAIN-MKPRU.csv', usecols=[1], engine='python', skipfooter=3)
+dataset = dataframe.values
 
 plt.figure()
 plt.plot(dataset,'g-',label='dwell')
 plt.legend(loc='best')
 plt.savefig('1.png')
-
-dataset=predict_value_with_exp_smoothing_3(data=dataset,params=[0.2,0.05,0.9],T=365,predict_step=100) 
-plt.figure()
-plt.plot(dataset,'g-',label='dwell',color='red')
-plt.legend(loc='best')
-plt.savefig('2.png')
 plt.close()
 
+# 将整型变为float
+dataset = dataset.astype('float32')
+#归一化 在下一步会讲解
+scaler = MinMaxScaler(feature_range=(0, 1))
+dataset = scaler.fit_transform(dataset)
 
+train_size = int(len(dataset) * 0.9)
+trainlist = dataset[:train_size]
+testlist = dataset[train_size:]
+
+def create_dataset(dataset, timestep):
+#这里的look_back与timestep相同
+    dataX, dataY = [], []
+    for i in range(len(dataset)-timestep-1):
+        a = dataset[i:(i+timestep)]
+        dataX.append(a)
+        dataY.append(dataset[i + timestep])
+    return np.array(dataX),np.array(dataY)
+#训练数据太少 timestep并不能过大
+timestep = 100
+trainX,trainY  = create_dataset(trainlist,timestep)
+testX,testY = create_dataset(testlist,timestep)
+
+trainX = np.reshape(trainX, (trainX.shape[0], trainX.shape[1], 1))
+testX = np.reshape(testX, (testX.shape[0], testX.shape[1] ,1 ))
+
+# create and fit the LSTM network
+model = Sequential()
+model.add(LSTM(4, input_shape=(None,1)))
+model.add(Dense(1))
+model.compile(loss='mean_squared_error', optimizer='adam')
+# model.fit(trainX, trainY, epochs=100, batch_size=100, verbose=2)
+# model.save(os.path.join("DATA","Test" + ".h5"))
+# make predictions
+
+model = load_model(os.path.join("DATA","Test" + ".h5"))
+trainPredict = model.predict(trainX)
+testPredict = model.predict(testX)
+
+#反归一化
+# trainPredict = scaler.inverse_transform(trainPredict)
+# trainY = scaler.inverse_transform(trainY)
+testPredict = scaler.inverse_transform(testPredict).tolist()
+# testY = scaler.inverse_transform(testY)
+
+trainlist=scaler.inverse_transform(trainlist).tolist()
+
+for i in range(len(testPredict)):
+    trainlist.append(testPredict[i])
+
+plt.figure()
+plt.plot(trainlist,'g-',label='dwell',color='red')
+plt.legend(loc='best')
+plt.savefig('2.png')
 
 
 
